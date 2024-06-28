d55268feeed649912f0c209998ef1c912e16de0c
La-Programmer <justinoghenekomeebedi@gmail.com>
2024-06-28 11:49:57 +0100

    Fix task 10

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -6,7 +6,11 @@ export default class Car {
       color,
     };
     for (const [key, value] of Object.entries(properties)) {
-      this[`${key}`] = value;
+      if (typeof value === 'string' || typeof value === 'undefined') {
+        this[`${key}`] = value;
+      } else {
+        throw new TypeError(`${key.charAt(0).toUpperCase() + key.slice(1)} must be a string`);
+      }
     }
   }
 

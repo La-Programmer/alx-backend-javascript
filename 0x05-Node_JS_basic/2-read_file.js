38752cbad95402f20ef96872f68714e6f53d226c
La-Programmer <justinoghenekomeebedi@gmail.com>
2024-08-20 10:43:28 +0100

    Fix task 2

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,54 +1,42 @@
-const fs = require('fs');
-const readLine = require('readline');
+const fs = require('node:fs');
 
-const getDataFromCsv = (path) => new Promise((resolve, reject) => {
-  const csvData = [];
-  const readStream = fs.createReadStream(path);
-  const readInterface = readLine.createInterface({
-    input: readStream,
-  });
-  readInterface.on('line', (line) => {
-    const trimmedLine = line.trim();
-    if (trimmedLine !== '') {
-      const row = trimmedLine.split(',');
-      csvData.push(row);
-    }
-  });
-  readInterface.on('close', () => {
-    resolve(csvData);
-  });
-  readInterface.on('error', () => {
-    reject(new Error('Cannot load the database'));
-  });
-});
-
-async function displayCsvData(path) {
+const getCsvData = (path) => {
   try {
-    const csvData = await getDataFromCsv(path);
-    const userCount = csvData.length - 1;
-    const fields = {};
-    for (const entry of csvData.slice(1)) {
-      const field = entry[entry.length - 1];
-      if (field in fields) {
-        fields[field].push(entry[0]);
-      } else {
-        fields[field] = [entry[0]];
-      }
-    }
-    console.log(`Number of students: ${userCount}`);
-    for (const key in fields) {
-      if (Object.keys(fields).includes(key)) {
-        const value = fields[key];
-        console.log(`Number of students in ${key}: ${value.length}. List: ${value.join(', ')}`);
-      }
-    }
+    const data = fs.readFileSync(path, 'utf-8');
+    return data;
   } catch (err) {
-    console.log(err.message);
+    throw new Error('Cannot load the database');
   }
-}
+};
+
+const display = (path) => {
+  const message = getCsvData(path);
+  const students = message.split('\n').slice(1);
+  const fields = {};
+  for (const entry of students) {
+    const keys = Object.keys(fields);
+    const splitEntry = entry.split(',');
+    const lastIndex = splitEntry.length - 1;
+    const field = splitEntry[lastIndex];
+    const firstName = splitEntry[0];
+    if (keys.includes(field)) {
+      fields[field].push(firstName);
+    } else {
+      fields[field] = [firstName];
+    }
+  }
+  console.log(`Number of students: ${students.length}`);
+  for (const key in fields) {
+    if (Object.keys(fields).includes(key)) {
+      const value = fields[key];
+      const sutdentNo = value.length;
+      console.log(`Number of students in ${key}: ${sutdentNo}. List: ${value.join(', ')}`);
+    }
+  }
+};
 
 const countStudents = (path) => {
-  displayCsvData(path);
+  display(path);
 };
 
 module.exports = countStudents;

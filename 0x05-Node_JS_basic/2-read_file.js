213a7c6e1e7df1699a0b9fdaae9da4f57b9e12e5
La-Programmer <justinoghenekomeebedi@gmail.com>
2024-08-20 11:07:14 +0100

    Fix task 2 again(4)

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -10,26 +10,26 @@ const getDataFromCsv = (path) => {
 };
 
 function displayCsvData(path) {
-    const csvRawData = getDataFromCsv(path);
-    const csvData = csvRawData.split('\n');
-    const userCount = csvData.length - 1;
-    const fields = {};
-    for (const rawEntry of csvData.slice(1)) {
-      const entry = rawEntry.split(',');
-      const field = entry[entry.length - 1];
-      if (field in fields) {
-        fields[field].push(entry[0]);
-      } else {
-        fields[field] = [entry[0]];
-      }
+  const csvRawData = getDataFromCsv(path);
+  const csvData = csvRawData.split('\n');
+  const userCount = csvData.length - 1;
+  const fields = {};
+  for (const rawEntry of csvData.slice(1)) {
+    const entry = rawEntry.split(',');
+    const field = entry[entry.length - 1];
+    if (field in fields) {
+      fields[field].push(entry[0]);
+    } else {
+      fields[field] = [entry[0]];
     }
-    console.log(`Number of students: ${userCount}`);
-    for (const key in fields) {
-      if (Object.keys(fields).includes(key)) {
-        const value = fields[key];
-        console.log(`Number of students in ${key}: ${value.length}. List: ${value.join(', ')}`);
-      }
+  }
+  console.log(`Number of students: ${userCount}`);
+  for (const key in fields) {
+    if (Object.keys(fields).includes(key)) {
+      const value = fields[key];
+      console.log(`Number of students in ${key}: ${value.length}. List: ${value.join(', ')}`);
     }
+  }
 }
 
 const countStudents = (path) => {

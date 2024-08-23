965008e931a61bc5fa2adaed5e4985acc91cb207
La-Programmer <justinoghenekomeebedi@gmail.com>
2024-08-23 10:50:31 +0100

    Fix test file 1

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -1,24 +1,55 @@
 const assert = require('assert');
-const { describe, it } = require('node:test');
 const calculateNumber = require('./1-calcul');
 
 describe('#calculateNumber()', function () {
-  it('should return the sum of its arguments', function () {
-    assert.equal(calculateNumber('SUM', 4, 4), 8);
-    assert.equal(calculateNumber('SUM', 4, 8), 12);
-    assert.equal(calculateNumber('SUM', 6, 14), 20);
-    assert.equal(calculateNumber('SUM', 1.4, 4.5), 6);
+  describe('when type is SUM', () => {
+    it('should round the first argument', () => {
+      assert.equal(calculateNumber('SUM', 10.2, 3), 13);
+      assert.equal(calculateNumber('SUM', -1.7, 8), 6);
+    });
+    it('should round the second argument', () => {
+      assert.equal(calculateNumber('SUM', 10, 3.1), 13);
+      assert.equal(calculateNumber('SUM', -2, 8.3), 6);
+    });
+    it('should return the right answer', () => {
+      assert.equal(calculateNumber('SUM', 13, 4), 17);
+      assert.equal(calculateNumber('SUM', 13, -44), -31);
+      assert.equal(calculateNumber('SUM', 13, 0), 13);
+    });
   });
-  it('should return the difference of its arguments', function () {
-    assert.equal(calculateNumber('SUBTRACT', 12, 9), 3);
-    assert.equal(calculateNumber('SUBTRACT', -12, 9), -21);
-    assert.equal(calculateNumber('SUBTRACT', 12, -19), 31);
-    assert.equal(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
+  describe('when type is SUBTRACT', () => {
+    it('should round the first argument', () => {
+      assert.equal(calculateNumber('SUBTRACT', 10.2, 3), 7);
+      assert.equal(calculateNumber('SUBTRACT', -1.7, 8), -10);
+    });
+    it('should round the second argument', () => {
+      assert.equal(calculateNumber('SUBTRACT', 10, 3.1), 7);
+      assert.equal(calculateNumber('SUBTRACT', -2, 8.3), -10);
+    });
+    it('should return the right answer', () => {
+      assert.equal(calculateNumber('SUBTRACT', 13, 4), 9);
+      assert.equal(calculateNumber('SUBTRACT', 13, -44), 57);
+      assert.equal(calculateNumber('SUBTRACT', 13, 0), 13);
+    });
   });
-  it('should return the quotient of its arguments', function () {
-    assert.equal(calculateNumber('DIVIDE', 12, 3), 4);
-    assert.equal(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
-    assert.equal(calculateNumber('DIVIDE', -400, 5), -80);
-    assert.equal(calculateNumber('DIVIDE', 1.4, 0), 'Error');
+  describe('when type is DIVIDE', () => {
+    it('should round the first argument', () => {
+      assert.equal(calculateNumber('DIVIDE', 10.2, 2), 5);
+      assert.equal(calculateNumber('DIVIDE', -18.3, 9), -2);
+    });
+    it('should round the second argument', () => {
+      assert.equal(calculateNumber('DIVIDE', 10, 2.1), 5);
+      assert.equal(calculateNumber('DIVIDE', 12, 3.4), 4);
+    });
+    it('should return the right answer', () => {
+      assert.equal(calculateNumber('DIVIDE', 40, 8), 5);
+      assert.equal(calculateNumber('DIVIDE', 144, 144), 1);
+      assert.equal(calculateNumber('DIVIDE', 121, -11), -11);
+    });
+    it('should return Error', () => {
+      assert.equal(calculateNumber('DIVIDE', 40, 0), 'Error');
+      assert.equal(calculateNumber('DIVIDE', 144, 0), 'Error');
+      assert.equal(calculateNumber('DIVIDE', 121, 0), 'Error');
+    });
   });
 });
